7ec5d1b50c7ab06c1e80cb260535fd989651ccc6
smarika <smarika.7sharma@gmail.com>
2018-11-12 22:44:21 +0530

    sclafmt

--- src/lectures/part1as/Recap.scala
+++ src/lectures/part1as/Recap.scala
@@ -9,17 +9,17 @@ object Recap extends App {
 
   // instructions vs expressions
   /* instructions are fundamental operative of meparitive programming languages like python, java, c++
-  * and are executed in order/sequence
-  *
-  * programs through expressions by building expressions on top of other expressions.
-  * */
-  val acondition:Boolean = false
+   * and are executed in order/sequence
+   *
+   * programs through expressions by building expressions on top of other expressions.
+   * */
+  val acondition: Boolean = false
 
-  val aconditionedVal = if(acondition) 42 else 56
-//compile infer types
-val aCodeBlock = {
-    if(acondition) 42 else 56
-}
+  val aconditionedVal = if (acondition) 42 else 56
+  //compile infer types
+  val aCodeBlock = {
+    if (acondition) 42 else 56
+  }
 
   //unit
 
@@ -27,7 +27,7 @@ val aCodeBlock = {
 
   //functions
 
-  def aFunction(x: Int) = x+1
+  def aFunction(x: Int) = x + 1
 
   //@tailrec
   /*we already know that tail recursion makes recursive functions not use additional stack frames when calling them recursively.
@@ -35,23 +35,22 @@ val aCodeBlock = {
 That used to cause a lot of stack overflow errors,But with Taylor rec they are converted to iterations in the jaivin by code.*/
 
   @tailrec
-  def factorial(n:Int, accumulator: Int):Int =
-  {
-    if(n>=0)accumulator else factorial(n-1,n*accumulator)
+  def factorial(n: Int, accumulator: Int): Int = {
+    if (n >= 0) accumulator else factorial(n - 1, n * accumulator)
   }
 
   //inheritance , the single class polymorphism
-//OO programming
+  //OO programming
   class Animal
   class Dog extends Animal
 
   val aDog: Animal = new Dog //subtyping polymorphism
 
-  trait Carnivore{
-    def eat(a: Animal) :Unit
+  trait Carnivore {
+    def eat(a: Animal): Unit
   }
 
-  class Crocodile extends Animal with Carnivore{
+  class Crocodile extends Animal with Carnivore {
     override def eat(a: Animal): Unit = println("food!")
   }
 
@@ -60,27 +59,25 @@ That used to cause a lot of stack overflow errors,But with Taylor rec they are c
   val aCroc = new Crocodile
   aCroc.eat(aDog)
 
-  aCroc eat(aDog)  //can also be written like this, natural language, compile will rewrite it like above line
+  aCroc eat (aDog) //can also be written like this, natural language, compile will rewrite it like above line
 
-//anonymous classes
+  //anonymous classes
 
-  val aCarnivore  = new Carnivore { //trait cannot be instantiated alone, let us provide a implementation on the spot to use it
-    //actually compiler creates a new anonymous class for us by extending carnivore and overriding the method either here on the spot
-    override def eat(a: Animal): Unit =  println("food food!!")
-  }
-
+  val aCarnivore =
+    new Carnivore { //trait cannot be instantiated alone, let us provide a implementation on the spot to use it
+      //actually compiler creates a new anonymous class for us by extending carnivore and overriding the method either here on the spot
+      override def eat(a: Animal): Unit = println("food food!!")
+    }
 
   //generics
 
-  abstract class MyList[+A]{
-
-  }
+  abstract class MyList[+A] {}
 
   object MyList
 
   //case class
 
-  case class Person(name: String, age :Int)
+  case class Person(name: String, age: Int)
 
   //execptions. try/catch/finally
 
@@ -88,41 +85,51 @@ That used to cause a lot of stack overflow errors,But with Taylor rec they are c
 
   val aPotentialFailure = try {
     throwsExecption
-  }
-  catch {
+  } catch {
     case e: Exception => println("execption")
-  }
-  finally{
+  } finally {
     println("some logs")
 
   }
-//functional programming
+  //functional programming
 
-  val incrementer =  new Function[Int,Int] {
+  val incrementer = new Function[Int, Int] {
     override def apply(v1: Int): Int = v1 + 1
   }
 
   incrementer(1)
 
+  val anonymousincrementor = (x: Int) => x + 1
 
-  val anonymousincrementor = (x:Int) => x + 1
-
-List(1,2,3).map(anonymousincrementor)
+  List(1, 2, 3).map(anonymousincrementor)
 
   //for comprehension
 
   val pairs = for {
-    listInt <- List(1,2,3)
-    listChar <- List('a','b')
-  }yield listChar + "-"+listInt // char and ints
+    listInt <- List(1, 2, 3)
+    listChar <- List('a', 'b')
+  } yield listChar + "-" + listInt // char and ints
 
   //scala collections, maps, Arrays, Lists, vectors, Maps, Tuples
 
-  val aMap = Map(1->"abc")
+  val aMap = Map(1 -> "abc")
 
   //collection Option, Try
+  val anOption = Some(2)
 
+  //pattern matching
+  val x = 2
 
+  val order = x match {
 
+    case 1 => "first"
+    case 2 => "second"
+  }
+
+  val person1 = Person("Bob", 22)
+
+  val greeting = person1 match {
+    case Person(n, _) => s"hi my name is $n"
+  }
 
 }
